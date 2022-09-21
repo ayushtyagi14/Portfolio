4e4c0a2fbf9d04e982b58422faa3379e355f7046
Ayush Tyagi <ayush.tyagi145@gmail.com>
2022-09-22 02:44:56 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
         Web Developer.  Coder.  Learner
       </SectionText>
       <Button onClick={() => window.location = '#connect'} >Let's Connect!</Button>
-      <Button onClick={() => window.location.href = 'https://drive.google.com/drive/folders/1WhPQjIM7_ZJARvntZnnQfaYR8Hqi_ujN'} >My Resume
+      <Button onClick={() => window.location.href = 'https://drive.google.com/file/d/1mrV5rVySVj0It_pCAi2uqQMwQimw7P2z/view?usp=sharing'} >My Resume
         <FiDownload />
       </Button>
     </LeftSection>
